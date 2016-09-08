f7b89f7168fc0888eb10d74fdd6389474d951764
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-08 15:35:11 +0300

    Require listBooks module in app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@
 
 require("../scss/style.scss")
 let {books} = require("../json/books.json")
+let listBooks = require("./listBooks.js")
 let bookList = listBooks(books)
 
 document.getElementsByClassName("books-page-content")[0].appendChild(bookList)
