6cc33c39f3c2037d3bd88571defc5affdcf493ba
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-08 15:20:45 +0300

    Add book listing to app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,6 @@
 
 require("../scss/style.scss")
 let {books} = require("../json/books.json")
+let bookList = listBooks(books)
+
+document.getElementsByClassName("books-page-content")[0].appendChild(bookList)
