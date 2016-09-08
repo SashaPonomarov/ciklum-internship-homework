1833e46e82d47198a06aea40c76d312430ba592b
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-08 15:26:14 +0300

    Implement movement of needed files to build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: __dirname + "/src/scripts/app.js",
     output: {
@@ -10,6 +12,13 @@ module.exports = {
             { test: /\.json$/, loader: "json" },
         ]
     },
+
+    plugins: [
+    new CopyWebpackPlugin([
+      {from: "src/index.html", to: "index.html"},
+      {from: "src/img/", to: "img/"}
+    ]),
+    ],
 
     watch: true
 };
