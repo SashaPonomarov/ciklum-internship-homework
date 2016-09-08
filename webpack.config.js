ad6e2117e7c7781fbeb9ce5e834f8db61666f318
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-08 15:39:41 +0300

    Add autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: __dirname + "/src/scripts/app.js",
@@ -8,7 +9,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.scss$/, loader: "style!css!sass" },
+            { test: /\.scss$/, loader: "style!css!postcss!sass" },
             { test: /\.json$/, loader: "json" },
         ]
     },
@@ -19,6 +20,8 @@ module.exports = {
       {from: "src/img/", to: "img/"}
     ]),
     ],
+
+    postcss: [ autoprefixer({ browsers: ['last 3 versions', '> 1%'] }) ],
 
     // watch: true
 };
