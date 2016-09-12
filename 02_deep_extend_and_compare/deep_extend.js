1a59fc00588a8ecd54e21e316d8b14517ba30819
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-12 16:30:16 +0300

    Update usage with compare, add semicolons

--- 02_deep_extend_and_compare/deep_extend.js
+++ 02_deep_extend_and_compare/deep_extend.js
@@ -1,25 +1,25 @@
 "use strict";
 
 function isObject (obj) {
-    return Object.prototype.toString.call(obj) === '[object Object]'
+    return Object.prototype.toString.call(obj) === '[object Object]';
 }
 
 function deepExtend(target) {
-    if (!isObject(target)) throw new TypeError('Target is (' + target + '), not an Object')
+    if (!isObject(target)) throw new TypeError('Target is (' + target + '), not an Object');
     for (let i = 1; i < arguments.length; i++)
         {
-            let from = arguments[i]
-            if (!isObject(from)) throw new TypeError('Argument ' + (i+1) + ' is (' + from + '), not an Object')
+            let from = arguments[i];
+            if (!isObject(from)) throw new TypeError('Argument ' + (i+1) + ' is (' + from + '), not an Object');
             Object.keys(from).forEach(function(key) {
                 if (isObject(target[key]) && isObject(from[key])) {
-                    deepExtend(target[key], from[key])
+                    deepExtend(target[key], from[key]);
                 }
                 else {
-                    target[key] = from[key]
+                    target[key] = from[key];
                 }
             })
         }
-    return target
+    return target;
 }
 
-module.exports = deepExtend
+module.exports = deepExtend;
--- 02_deep_extend_and_compare/usage.js
+++ 02_deep_extend_and_compare/usage.js
@@ -1,8 +1,28 @@
-const deepExtend = require("./deep_extend.js")
+const deepExtend = require("./deep_extend.js");
 
-console.log('testing deep extend:')
+console.log('testing deep extend:');
 
-let o1 = {a: 1, b: 'some string', c: {a: 10, b: 11, c: 12}}
-let o2 = {a: 2, b: [1, 2, 3], c: {d: 9}, x: {y: null}}
+let o1 = {a: 1, b: 'some string', c: {a: 10, b: 11, c: 12}};
+let o2 = {a: 2, b: [1, 2, 3], c: {d: 9}, x: {y: null}};
 
-console.log(deepExtend(o1, o2))
+console.log(deepExtend(o1, o2));
+
+
+
+
+const deepCompare = require("./deep_compare.js");
+
+console.log('testing deep compare:');
+
+o1 = {a: 1, b: 'some string', c: null, d: {a: 1}, arr: [1, 2, 3]};
+o2 = {a: 1, b: 'some string', c: null, d: {a: 1}, arr: [1, 2, 3]};
+
+console.log(o1 + ' = '  + o2);
+console.log(deepCompare(o1, o2)); //true
+
+
+o1 = {a: 1, b: {}, c: null, d: {a: 1}, arr: [1, 2, 3]};
+o2 = {a: 1, b: 'some string', c: null, d: {a: 1}, arr: [1, 2, 3]};
+
+console.log(o1 + ' = '  + o2);
+console.log(deepCompare(o1, o2)); //false
