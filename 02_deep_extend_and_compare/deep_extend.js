0afa1cf11f523df1d0027e96fa545acb2c602b46
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-12 14:50:59 +0300

    Add isObject() helper function

--- 02_deep_extend_and_compare/deep_extend.js
+++ 02_deep_extend_and_compare/deep_extend.js
@@ -1,13 +1,14 @@
 "use strict";
 
-
+function isObject (obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]'
+}
 
 function deepExtend(target) {
     for (let i = 1; i < arguments.length; i++)
         {
             let from = arguments[i]
             Object.keys(from).forEach(function(key) {
-
                     target[key] = from[key]
             })
         }
