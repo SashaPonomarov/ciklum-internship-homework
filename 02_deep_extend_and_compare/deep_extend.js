2782b62a323fd277314ec0304954f9c0275fb450
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-12 14:49:35 +0300

    Add shallow extend function

---
+++ 02_deep_extend_and_compare/deep_extend.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+
+function deepExtend(target) {
+    for (let i = 1; i < arguments.length; i++)
+        {
+            let from = arguments[i]
+            Object.keys(from).forEach(function(key) {
+
+                    target[key] = from[key]
+            })
+        }
+    return target
+}
+
+module.exports = deepExtend
