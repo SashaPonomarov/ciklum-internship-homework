6f16422f5233685b01fa436502a2a1215c27890f
Sasha Ponomarov <randirsbox@gmail.com>
2016-09-22 13:38:08 +0300

    Add localstorage saving to Task 05

--- 05_omdb_angular/js/app.js
+++ 05_omdb_angular/js/app.js
@@ -8,6 +8,14 @@ import angular from "angular"
 angular.module("omdb-search", [])
     .controller("SearchMovies", function($scope, $http) {
 
+        let stored = localStorage.getItem('selected')
+        if (stored) {
+            $scope.selected = JSON.parse(stored)
+        } else {
+            $scope.selected = []
+        }
+        console.log('selected', $scope.selected)        
+
         $scope.search = function() {
           $http.get("http://www.omdbapi.com/?s=" + $scope.searchInput)
           .then(function(response){ 
@@ -16,12 +24,27 @@ angular.module("omdb-search", [])
         }
 
         $scope.select = function(imdbId) {
-            $scope.selected = $scope.selected || []
             let current = $scope.movies.Search.find((movie) => {
                 return movie.imdbID === imdbId
             })
             $scope.selected.push(current)
+        }
+
+        $scope.deselect = function(imdbId) {
+            $scope.selected = $scope.selected.filter((movie) => {
+                return movie.imdbID !== imdbId
+            })
+        }
+
+        $scope.$watch('selected', function() {
             console.log($scope.selected)
+            localStorage.setItem('selected', JSON.stringify($scope.selected))
+        }, true)
+
+        $scope.notSelected = function(movie) {
+            return !$scope.selected.some((select) => {
+                return select.imdbID === movie.imdbID
+            })
         }
 
     })
