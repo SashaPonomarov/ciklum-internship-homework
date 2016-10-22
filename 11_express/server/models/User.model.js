da8a779d5e393e6d3fb225433414ec6d73984095
Sasha Ponomarov <randirsbox@gmail.com>
2016-10-22 23:09:39 +0300

    Task 11, Update User model

--- 11_express/server/models/User.model.js
+++ 11_express/server/models/User.model.js
@@ -6,7 +6,7 @@ const User = new Schema({
   username: String,
   password: String,
   favorites: [{
-    imdbID: { type : String , unique : true },
+    imdbID: String,
     Title: String,
     Poster: String
   }]
